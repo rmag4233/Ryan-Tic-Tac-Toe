a936b544a852d24cfc3e94ef54c147853d493c89
rmag4233 <ryan.agnew@libertymutual.com>
2017-09-27 11:20:15 -0400

    Add production environment
    Added fake production environment

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,8 @@
 
 const config = {
   apiOrigins: {
-    development: 'http://tic-tac-toe.wdibos.com'
+    development: 'http://tic-tac-toe.wdibos.com',
+    production: 'http://tic-tac-toe.wdibos.com'
   }
 }
 
