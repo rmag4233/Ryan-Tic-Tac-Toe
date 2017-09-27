4ccde7549ce335b15e218a52e1a2826971424072
rmag4233 <ryan.agnew@libertymutual.com>
2017-09-27 14:28:28 -0400

    Add Sign In Functionality
    Adding sign in functionality from HTML to UI.

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const store = require('./store')
+
 const signUpSuccess = function (data) {
   $('#messageContent').text('You have signed up as ' + data.user.email + '. Sign in to start playing!')
   console.log(data)
@@ -9,7 +11,21 @@ const signUpFailure = function (error) {
   console.error(error)
 }
 
+const signInSuccess = function (data) {
+  console.log('Success!')
+  console.log(data)
+  $('#signInMessage').text('Signed in as ' + data.user.email)
+  store.user = data.user
+}
+
+const signInFailure = function (error) {
+  console.error(error)
+  $('#signInMessage').text('Please try signing in with a registered email and password.')
+}
+
 module.exports = {
   signUpSuccess,
-  signUpFailure
+  signUpFailure,
+  signInSuccess,
+  signInFailure
 }
---
+++ scripts/sign-in-json.sh
@@ -0,0 +1,18 @@
+# EMAIL=m@m.com PASSWORD=map sh scripts/sign-up-json.sh
+#`{"user":{"id":853,"email":"m@m.com"}}`
+
+API="${API_ORIGIN:-http://tic-tac-toe.wdibos.com}"
+URL_PATH="/sign-in"
+
+curl "${API}${URL_PATH}" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "credentials": {
+      "email": "'"${EMAIL}"'",
+      "password": "'"${PASSWORD}"'"
+    }
+  }'
+
+echo
