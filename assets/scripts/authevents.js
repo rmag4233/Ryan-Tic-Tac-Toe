430cff78b1913c796b78eff01afbc89cb9a1afce
rmag4233 <ryan.agnew@libertymutual.com>
2017-09-27 11:08:53 -0400

    Resolved file connection issues
    Resolved issues with file connections

--- assets/scripts/authevents.js
+++ assets/scripts/authevents.js
@@ -1,16 +1,17 @@
 'use strict'
 
-const getFormFields = require(`../../../lib/get-form-fields`)
+const getFormFields = require(`../../lib/get-form-fields`)
 
 const api = require('./api')
-const ui = require('./ui')
+// const ui = require('./ui')
 
 const onSignUp = function (event) {
   const data = getFormFields(this)
   event.preventDefault()
   api.signUp(data)
-    .then(ui.signUpSuccess)
-    .catch(ui.signUpFailure)
+  console.log(data)
+  // .then(ui.signUpSuccess)
+  // .catch(ui.signUpFailure)
 }
 
 const authHandlers = function () {
